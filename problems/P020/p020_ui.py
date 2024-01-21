f7fbd645cf6892f8ca2d0d4b8e6a55a60b127fb2
AbbasSaberi <abbasbazicomputer@gmail.com>
2024-01-21 17:53:38 +0330

    Added p020_ui

--- problems/P020/p020_ui.py
+++ problems/P020/p020_ui.py
@@ -1,2 +1,32 @@
 from nicegui import ui
 from p020_solution import P020
+
+class Options:
+    def __init__(self):
+        self.visible = False 
+        self.first = True
+
+options = Options()
+
+def show_ui(visible):
+    options.visible = visible
+    if options.first:
+        options.first = False
+        root = create_ui()
+    
+def create_ui():
+    with ui.column().bind_visibility_from(options, 'visible') as root:
+        numberInput = ui.input('Number')
+        ui.button('Check Number', on_click=lambda: check_number())
+        output = ui.label()
+
+    def check_number():
+        number = int(numberInput.value)
+        result = P020(number)
+        output.text = result
+
+    return root
+
+if __name__ in {"__main__", "__mp_main__"}:
+    show_ui(True)
+    ui.run()
